0560a57ef7098a7182383b304e90097197048449
Phantom Go <gfp88316@gmail.com>
2016-12-07 22:53:29 +0800

    4publish

--- main.go
+++ main.go
@@ -16,7 +16,8 @@ func main() {
 	mux.HandleFunc(weixin.MsgTypeText, Echo)
 	// 注册关注事件的处理函数
 	mux.HandleFunc(weixin.MsgTypeEventSubscribe, Subscribe)
-	http.Handle("/", mux)                  // 注册接收微信服务器数据的接口URI
+	http.Handle("/", mux) // 注册接收微信服务器数据的接口URI
+	go CreateMenu(mux)
 	err := http.ListenAndServe(":80", nil) // 启动接收微信数据服务器
 	if err != nil {
 		fmt.Println(err)
